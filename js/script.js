6da0a3d439ec944e30fbdf57bd6d981a335ac7cd
EMCodeStudio <estivenmc53@gmail.com>
2023-04-29 12:03:47 -0500

    update search

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const btnMenu = document.getElementById('btn-menu');
 const menuRight = document.getElementById('right-menu');
 const btnClose = document.getElementById('btn-close');
+const searchButton = document.querySelector('.btn-search');
+const searchContainer = document.querySelector('.search-container');
 
 btnMenu.addEventListener('click', () => {
     menuRight.classList.toggle('open');
 });
 
-
-
 btnClose.addEventListener('click', () => {
     menuRight.classList.remove('open');
 });
+
+searchButton.addEventListener('click', () => {
+    searchContainer.classList.toggle('show');
+  });
